225712ec6ea704e8b0e4542a309afa898ff93521
Ronnie <digiron242@gmail.com>
2023-06-09 23:04:59 +1000

    added shameless github plug in batchfile

--- launch.bat
+++ launch.bat
@@ -1,2 +1,2 @@
 @ECHO OFF
-IF EXIST PowerShell\pwsh.exe (start "PowerShell (Portable)" PowerShell\pwsh.exe -noexit -file .\PSLoader.ps1) ELSE (start "PowerShell (Local+)" powershell -noexit -file .\PSLoader.ps1)
+IF EXIST PowerShell\pwsh.exe (start "PowerShell (Portable) - github.com/digi-ron" PowerShell\pwsh.exe -noexit -file .\PSLoader.ps1) ELSE (start "PowerShell (Local+) - github.com/digi-ron" powershell -noexit -file .\PSLoader.ps1)
